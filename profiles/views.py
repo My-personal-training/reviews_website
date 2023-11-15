00760579ee323c26226eb3d13cd46eec61a791e4
Lorenzo Arias Villegas <santyago.arias.sav@gmail.com>
2023-11-15 00:43:50 -0400

    First commit

---
+++ profiles/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+
+from .forms import ProfileForm
+from .models import UserProfile
+
+
+class CreateProfileView(CreateView):  # One of the best ways and most common ways to create a form
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = ["user_image"]  # This also can be like: fields = "__all__"
+    success_url = "/profiles"
+
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
+#
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+#
+#         if submitted_form.is_valid():
+#             profile = UserProfile(user_image =request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+#
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
+
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
